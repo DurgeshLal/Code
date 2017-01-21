1ec13d5ea20382440908db3efc0d0691638653b3
Durgesh Gupta <durgesh.cs.06@live.com>
2017-01-20 20:04:29 -0600

    Initial Commit

---
+++ JPMBManageAlerts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JPMBManageAlerts
+//
+//  Created by Durgesh Lal Gupta on 1/20/17.
+//  Copyright © 2017 Durgesh Lal Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
